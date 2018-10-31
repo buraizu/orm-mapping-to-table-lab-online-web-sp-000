34f131eee339a3dbd9ac9bd24c9884ba60111057
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-31 22:50:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,6 +28,12 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.create(name:, grade:)
+    student = self.new(name, grade)
+    student.save
+    student 
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
